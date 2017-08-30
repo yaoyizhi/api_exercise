cf71852864e78ca5a52be0b5799c57710ac20f9a
yaoyizhi <email@yaoadou888>
2017-08-30 09:49:48 +0800

    Jbuilder 用法

--- app/controllers/api/v1/trains_controller.rb
+++ app/controllers/api/v1/trains_controller.rb
@@ -1,19 +1,9 @@
 class Api::V1::TrainsController < ApiController
   def index
-    @trains = Train.all
-    render json: {
-      data: @trains.map do |train|
-              { number: train.number, train_url: api_v1_train_url(train.number) }
-            end
-    }
+    @trains = Train.paginate(page: params[:page])
   end
 
   def show
     @train = Train.find_by_number!(params[:train_number])
-
-    render json: {
-      number: @train.number,
-      available_seats: @train.available_seats
-    }
   end
 end
